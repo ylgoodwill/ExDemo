85878a52b049417b33de1d1f50037b397758e85d
unknown <>
2017-12-29 17:50:49 +0800

    add the function of wordrecognition

--- app/src/main/java/com/ActivityMain.java
+++ app/src/main/java/com/ActivityMain.java
@@ -1,4 +1,4 @@
-package com.voicerecognition.android.voicedemo.activity;
+package com;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,18 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import com.baidu.speech.recognizerdemo.R;
+import com.voicerecognition.android.voicedemo.activity.ActivityOnline;
+import com.wordrecognition.MainActivity;
 
 public class ActivityMain extends AppCompatActivity {
     Button voice_Re;
+    Button word_Re;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         voice_Re = (Button) findViewById(R.id.voice_re);
+        word_Re = (Button) findViewById(R.id.word_re);
         voice_Re.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ActivityMain.this,ActivityOnline.class));
+            }
+        });
+        word_Re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityMain.this,MainActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/wordrecognition/ui/camera/PermissionCallback.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.wordrecognition.ui.camera;
+
+public interface PermissionCallback {
+    boolean onRequestPermission();
+}
